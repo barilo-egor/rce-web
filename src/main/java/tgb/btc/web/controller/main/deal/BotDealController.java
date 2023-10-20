dc9e65f6e86b17825e764ba254401bd7dee221e1
barilo <e.barilo@isida.by>
2023-10-20 15:48:52 +0300

    relocate

---
+++ src/main/java/tgb/btc/web/controller/main/deal/BotDealController.java
@@ -0,0 +1,36 @@
+package tgb.btc.web.controller.main.deal;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tgb.btc.library.repository.bot.DealRepository;
+import tgb.btc.library.util.web.JacksonUtil;
+import tgb.btc.web.constant.ControllerMapping;
+import tgb.btc.web.constant.mapper.DealMapper;
+import tgb.btc.web.service.WebDealService;
+
+@RestController
+@RequestMapping(ControllerMapping.BOT_DEAL)
+public class BotDealController {
+
+    private WebDealService webDealService;
+
+    private DealRepository dealRepository;
+
+    @Autowired
+    public void setDealRepository(DealRepository dealRepository) {
+        this.dealRepository = dealRepository;
+    }
+
+    @Autowired
+    public void setWebDealService(WebDealService webDealService) {
+        this.webDealService = webDealService;
+    }
+
+    @GetMapping("/findAll")
+    public ObjectNode findAll(Integer page, Integer limit, Integer start) {
+        return JacksonUtil.pagingData(webDealService.findAll(page, limit, start), dealRepository.count(), DealMapper.FIND_ALL);
+    }
+}
