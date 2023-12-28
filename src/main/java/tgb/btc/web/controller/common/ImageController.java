c634535efc27575b2e32b8465b897fdc591c47bd
barilo <barilo>
2023-12-28 21:54:26 +0300

    refact

--- src/main/java/tgb/btc/web/controller/common/ImageController.java
+++ src/main/java/tgb/btc/web/controller/common/ImageController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import tgb.btc.api.bot.IFileDownloader;
 import tgb.btc.web.service.process.FileService;
 
 import java.io.IOException;
