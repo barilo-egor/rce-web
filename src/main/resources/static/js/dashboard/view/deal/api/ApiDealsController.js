8df623353b210a1fc26323971393a5c593735be3
barilo <barilo>
2024-06-03 22:17:40 +0300

    apiDealsGrid refactor

--- src/main/resources/static/js/dashboard/view/deal/api/ApiDealsController.js
+++ src/main/resources/static/js/dashboard/view/deal/api/ApiDealsController.js
@@ -65,5 +65,54 @@ Ext.define('Dashboard.view.deal.api.ApiDealsController', {
 
     clearFilterForm: function(me) {
         Ext.getStore('apiDealStore').fieldsReferences.forEach(field => ExtUtil.referenceQuery(field).clearValue())
+    },
+
+    dealsExport: function (me) {
+        ExtUtil.mRequest({
+            url: '/deal/api/beforeExport',
+            jsonData: Ext.getStore('apiDealStore').getFiltersFromPanel(),
+            success: function (response) {
+                if (response.body.data.success) {
+                    window.open('/deal/api/export')
+                } else {
+                    ExtMessages.topToast('Ошибка при экспорте сделок.')
+                }
+            }
+        })
+    },
+
+    gridMenu: function (me, eObj) {
+        me.deselectAll();
+        me.setSelection(eObj.record);
+        if (!me.menu) {
+            me.menu = Ext.create('Dashboard.view.deal.api.ApiDealsGridMenu')
+        }
+        me.menu.setViewModel({
+            data: {
+                deal: eObj.record
+            }
+        })
+        me.menu.showAt(eObj.event.getX(), eObj.event.getY());
+        eObj.event.stopEvent()
+    },
+    selectDeal: function (me, selected) {
+        ExtUtil.mask('apiUserInfoPanel', 'Обновление данных')
+        let user = selected[0].getData().user
+        ExtUtil.referenceQuery('chooseDealContainer').setHidden(true)
+        ExtUtil.referenceQuery('userInfoFieldsContainer').setHidden(false)
+        ExtUtil.referenceQuery('idDisplayField').setValue(user.id)
+        ExtUtil.referenceQuery('dealsCountDisplayField').setValue(user.dealsCount)
+        ExtUtil.referenceQuery('isBannedDisplayField').setValue(user.isBanned ? 'Да' : 'Нет')
+        ExtUtil.referenceQuery('personalDiscountDisplayField').setValue((user.personalDiscount ? user.personalDiscount : '0') + '%')
+        ExtUtil.referenceQuery('buyRequisiteDisplayField').setValue(user.buyRequisite ? user.buyRequisite : 'Отсутствует')
+        ExtUtil.referenceQuery('sellRequisiteDisplayField').setValue(user.sellRequisite ? user.sellRequisite : 'Отсутствует')
+        ExtUtil.referenceQuery('bynUsdCourseDisplayField').setValue(user.bynUsdCourse ? user.bynUsdCourse : 'Отсутствует')
+        ExtUtil.referenceQuery('rubUsdCourseDisplayField').setValue(user.rubUsdCourse ? user.rubUsdCourse : 'Отсутствует')
+        ExtUtil.referenceQuery('registrationDateDisplayField').setValue(user.registrationDate)
+        ExtUtil.maskOff('apiUserInfoPanel')
+    },
+
+    loadStore: function(me) {
+        me.getStore().load()
     }
 })
--- src/main/resources/static/js/dashboard/view/deal/api/ApiDealsGrid.js
+++ src/main/resources/static/js/dashboard/view/deal/api/ApiDealsGrid.js
@@ -2,38 +2,25 @@ Ext.define('Dashboard.view.deal.api.ApiDealsGrid', {
     extend: 'Ext.Panel',
     xtype: 'apidealsgrid',
     reference: 'apiDealsGrid',
-
     requires: [
         'Dashboard.view.deal.api.ApiDealsController',
         'Dashboard.view.deal.api.ApiDealsGridMenu'
     ],
     controller: 'apiDealsController',
 
-    flex: 1,
     shadow: true,
     margin: '5 5 10 10',
-    layout: 'fit',
     tbar: {
         items: [
             {
                 iconCls: 'x-fa fa-file-excel darkGreen',
                 tooltip: 'Экспорт сделок в Excel',
-                handler: function (me) {
-                    ExtUtil.mRequest({
-                        url: '/deal/api/beforeExport',
-                        jsonData: Ext.getStore('apiDealStore').getFiltersFromPanel(),
-                        success: function (response) {
-                            if (response.body.data.success) {
-                                window.open('/deal/api/export')
-                            } else {
-                                ExtMessages.topToast('Ошибка при экспорте сделок.')
-                            }
-                        }
-                    })
-                }
+                handler: 'dealsExport'
             }
         ]
     },
+
+    layout: 'fit',
     items: [
         {
             xtype: 'grid',
@@ -43,39 +30,9 @@ Ext.define('Dashboard.view.deal.api.ApiDealsGrid', {
                 pagingtoolbar: true
             },
             listeners: {
-                childcontextmenu: function (me, eObj) {
-                    me.deselectAll();
-                    me.setSelection(eObj.record);
-                    if (!me.menu) {
-                        me.menu = Ext.create('Dashboard.view.deal.api.ApiDealsGridMenu')
-                    }
-                    me.menu.setViewModel({
-                        data: {
-                            deal: eObj.record
-                        }
-                    })
-                    me.menu.showAt(eObj.event.getX(), eObj.event.getY());
-                    eObj.event.stopEvent()
-                },
-                select: function (me, selected) {
-                    ExtUtil.mask('apiUserInfoPanel', 'Обновление данных')
-                    let user = selected[0].getData().user
-                    ExtUtil.referenceQuery('chooseDealContainer').setHidden(true)
-                    ExtUtil.referenceQuery('userInfoFieldsContainer').setHidden(false)
-                    ExtUtil.referenceQuery('idDisplayField').setValue(user.id)
-                    ExtUtil.referenceQuery('dealsCountDisplayField').setValue(user.dealsCount)
-                    ExtUtil.referenceQuery('isBannedDisplayField').setValue(user.isBanned ? 'Да' : 'Нет')
-                    ExtUtil.referenceQuery('personalDiscountDisplayField').setValue((user.personalDiscount ? user.personalDiscount : '0') + '%')
-                    ExtUtil.referenceQuery('buyRequisiteDisplayField').setValue(user.buyRequisite ? user.buyRequisite : 'Отсутствует')
-                    ExtUtil.referenceQuery('sellRequisiteDisplayField').setValue(user.sellRequisite ? user.sellRequisite : 'Отсутствует')
-                    ExtUtil.referenceQuery('bynUsdCourseDisplayField').setValue(user.bynUsdCourse ? user.bynUsdCourse : 'Отсутствует')
-                    ExtUtil.referenceQuery('rubUsdCourseDisplayField').setValue(user.rubUsdCourse ? user.rubUsdCourse : 'Отсутствует')
-                    ExtUtil.referenceQuery('registrationDateDisplayField').setValue(user.registrationDate)
-                    ExtUtil.maskOff('apiUserInfoPanel')
-                },
-                painted: function(me) {
-                    me.getStore().load()
-                }
+                childcontextmenu: 'gridMenu',
+                select: 'selectDeal',
+                painted: 'loadStore'
             },
             columns: [
                 {
