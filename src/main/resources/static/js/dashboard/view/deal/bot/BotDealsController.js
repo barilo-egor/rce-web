8f6fb97e30150db4317c52383b9df3f5a42a2735
barilo <e.barilo@isida.by>
2024-06-03 17:28:40 +0300

    refactor BotDealsGrid.js

--- src/main/resources/static/js/dashboard/view/deal/bot/BotDealsController.js
+++ src/main/resources/static/js/dashboard/view/deal/bot/BotDealsController.js
@@ -66,5 +66,63 @@ Ext.define('Dashboard.view.deal.bot.BotDealsController', {
 
     clearFilterForm: function(me) {
         Ext.getStore('botDealStore').fieldsReferences.forEach(field => ExtUtil.referenceQuery(field).clearValue())
+    },
+
+    manualAddDeal: function (me) {
+        Ext.create('Dashboard.view.deal.bot.add.AddDialog').show()
+    },
+
+    exportDeals: function (me) {
+        ExtUtil.mRequest({
+            url: '/deal/bot/beforeExport',
+            jsonData: Ext.getStore('botDealStore').getFiltersFromPanel(),
+            success: function (response) {
+                if (response.body.data.success) {
+                    window.open('/deal/bot/export')
+                } else {
+                    ExtMessages.topToast('Ошибка при экспорте сделок.')
+                }
+            }
+        })
+    },
+
+    openGridMenu: function (me, eObj) {
+        me.deselectAll();
+        me.setSelection(eObj.record);
+        if (!me.menu) {
+            me.menu = Ext.create('Dashboard.view.deal.bot.BotDealsGridMenu')
+        }
+        me.menu.setViewModel({
+            data: {
+                deal: eObj.record
+            }
+        })
+        me.menu.showAt(eObj.event.getX(), eObj.event.getY());
+        eObj.event.stopEvent()
+    },
+
+    selectDeal: function (me, selected) {
+        ExtUtil.mask('botUserInfoPanel', 'Обновление данных')
+        let user = selected[0].getData().user
+        ExtUtil.referenceQuery('chooseDealContainer').setHidden(true)
+        ExtUtil.referenceQuery('userInfoFieldsContainer').setHidden(false)
+        ExtUtil.referenceQuery('chatIdDisplayField').setValue(user.chatId)
+        ExtUtil.referenceQuery('usernameDisplayField').setValue(user.username ? user.username : 'Скрыт')
+        ExtUtil.referenceQuery('banDisplayField').setValue(user.banned ? 'Да' : 'Нет')
+        ExtUtil.referenceQuery('fromChatIdDisplayField').setValue(user.fromChatId ? user.FromChatId : 'Отсутствует')
+        ExtUtil.referenceQuery('referralBalanceDisplayField').setValue((user.referralBanalnce ? user.referralBanalnce : '0') + 'р.')
+        ExtUtil.referenceQuery('referralPercentDisplayField').setValue(user.referralPercent + "%")
+        ExtUtil.referenceQuery('isRankDiscountOnDisplayField').setValue(user.rankDiscountOn ? "Включена" : "Выключена")
+        ExtUtil.referenceQuery('personalBuyDisplayField').setValue((user.personalBuy ? user.personalBuy : "0") + "%")
+        ExtUtil.referenceQuery('personalSellDisplayField').setValue((user.personalSell ? user.personalSell : "0") + "%")
+        ExtUtil.referenceQuery('referralUsersCountDisplayField').setValue(user.referralUsersCount + " рефералов")
+        ExtUtil.referenceQuery('isActiveDisplayField').setValue(user.active === false ? 'Нет' : 'Да')
+        ExtUtil.referenceQuery('dealsCountDisplayField').setValue(user.dealsCount + " сделок")
+        ExtUtil.referenceQuery('registrationDateDisplayField').setValue(user.registrationDate)
+        ExtUtil.maskOff('botUserInfoPanel')
+    },
+
+    loadStore: function(me) {
+        me.getStore().load()
     }
 })
--- src/main/resources/static/js/dashboard/view/deal/bot/BotDealsGrid.js
+++ src/main/resources/static/js/dashboard/view/deal/bot/BotDealsGrid.js
@@ -19,26 +19,12 @@ Ext.define('Dashboard.view.deal.bot.BotDealsGrid', {
             {
                 iconCls: 'x-fa fa-plus forestgreenColor',
                 tooltip: 'Добавление ручных сделок',
-                handler: function (me) {
-                    Ext.create('Dashboard.view.deal.bot.add.AddDialog').show()
-                }
+                handler: 'manualAddDeal'
             },
             {
                 iconCls: 'x-fa fa-file-excel darkGreen',
                 tooltip: 'Экспорт сделок в Excel',
-                handler: function (me) {
-                    ExtUtil.mRequest({
-                        url: '/deal/bot/beforeExport',
-                        jsonData: Ext.getStore('botDealStore').getFiltersFromPanel(),
-                        success: function (response) {
-                            if (response.body.data.success) {
-                                window.open('/deal/bot/export')
-                            } else {
-                                ExtMessages.topToast('Ошибка при экспорте сделок.')
-                            }
-                        }
-                    })
-                }
+                handler: 'exportDeals'
             }
         ]
     },
@@ -51,43 +37,9 @@ Ext.define('Dashboard.view.deal.bot.BotDealsGrid', {
                 pagingtoolbar: true
             },
             listeners: {
-                childcontextmenu: function (me, eObj) {
-                    me.deselectAll();
-                    me.setSelection(eObj.record);
-                    if (!me.menu) {
-                        me.menu = Ext.create('Dashboard.view.deal.bot.BotDealsGridMenu')
-                    }
-                    me.menu.setViewModel({
-                        data: {
-                            deal: eObj.record
-                        }
-                    })
-                    me.menu.showAt(eObj.event.getX(), eObj.event.getY());
-                    eObj.event.stopEvent()
-                },
-                select: function (me, selected) {
-                    ExtUtil.mask('botUserInfoPanel', 'Обновление данных')
-                    let user = selected[0].getData().user
-                    ExtUtil.referenceQuery('chooseDealContainer').setHidden(true)
-                    ExtUtil.referenceQuery('userInfoFieldsContainer').setHidden(false)
-                    ExtUtil.referenceQuery('chatIdDisplayField').setValue(user.chatId)
-                    ExtUtil.referenceQuery('usernameDisplayField').setValue(user.username ? user.username : 'Скрыт')
-                    ExtUtil.referenceQuery('banDisplayField').setValue(user.banned ? 'Да' : 'Нет')
-                    ExtUtil.referenceQuery('fromChatIdDisplayField').setValue(user.fromChatId ? user.FromChatId : 'Отсутствует')
-                    ExtUtil.referenceQuery('referralBalanceDisplayField').setValue((user.referralBanalnce ? user.referralBanalnce : '0') + 'р.')
-                    ExtUtil.referenceQuery('referralPercentDisplayField').setValue(user.referralPercent + "%")
-                    ExtUtil.referenceQuery('isRankDiscountOnDisplayField').setValue(user.rankDiscountOn ? "Включена" : "Выключена")
-                    ExtUtil.referenceQuery('personalBuyDisplayField').setValue((user.personalBuy ? user.personalBuy : "0") + "%")
-                    ExtUtil.referenceQuery('personalSellDisplayField').setValue((user.personalSell ? user.personalSell : "0") + "%")
-                    ExtUtil.referenceQuery('referralUsersCountDisplayField').setValue(user.referralUsersCount + " рефералов")
-                    ExtUtil.referenceQuery('isActiveDisplayField').setValue(user.active === false ? 'Нет' : 'Да')
-                    ExtUtil.referenceQuery('dealsCountDisplayField').setValue(user.dealsCount + " сделок")
-                    ExtUtil.referenceQuery('registrationDateDisplayField').setValue(user.registrationDate)
-                    ExtUtil.maskOff('botUserInfoPanel')
-                },
-                painted: function(me) {
-                    me.getStore().load()
-                }
+                childcontextmenu: 'openGridMenu',
+                select: 'selectDeal',
+                painted: 'loadStore'
             },
             columns: [
                 {
