9956e710b470e13069e4c32860b37b15ccf6ab50
barilo <e.barilo@isida.by>
2024-06-03 17:22:08 +0300

    refactor collapsible

--- src/main/resources/static/js/util/ExtUtilConfig.js
+++ src/main/resources/static/js/util/ExtUtilConfig.js
@@ -1,9 +1,35 @@
 const ExtUtilConfig = {
+    SUPPORT: {
+        COLLAPSIBLE_OBJECTS: {
+            TOP: {
+                direction: 'top',
+                expandToolText: 'Развернуть',
+                collapseToolText: 'Свернуть'
+            },
+            RIGHT: {
+                direction: 'right',
+                expandToolText: 'Развернуть',
+                collapseToolText: 'Свернуть'
+            },
+            LEFT: {
+                direction: 'left',
+                expandToolText: 'Развернуть',
+                collapseToolText: 'Свернуть'
+            },
+            BOTTOM: {
+                direction: 'bottom',
+                expandToolText: 'Развернуть',
+                collapseToolText: 'Свернуть'
+            }
+        }
+    },
     getCollapsible(direction) {
-        return {
-            direction: direction,
-            expandToolText: 'Развернуть',
-            collapseToolText: 'Свернуть'
+        debugger
+        switch (direction) {
+            case 'top': return this.SUPPORT.COLLAPSIBLE_OBJECTS.TOP
+            case 'right': return this.SUPPORT.COLLAPSIBLE_OBJECTS.RIGHT
+            case 'left': return this.SUPPORT.COLLAPSIBLE_OBJECTS.LEFT
+            case 'bottom': return this.SUPPORT.COLLAPSIBLE_OBJECTS.BOTTOM
         }
     }
 }
