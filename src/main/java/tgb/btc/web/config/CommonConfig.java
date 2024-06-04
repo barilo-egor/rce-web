26a6fb87986556f16aa06b4ba7d71f17d6b4c49c
barilo <e.barilo@isida.by>
2024-06-04 14:22:19 +0300

    close SSE after session destroyed

---
+++ src/main/java/tgb/btc/web/config/CommonConfig.java
@@ -0,0 +1,15 @@
+package tgb.btc.web.config;
+
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
+
+@Configuration
+public class CommonConfig {
+
+    @Bean
+    public ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
+        return new ServletListenerRegistrationBean<HttpSessionEventPublisher>(new HttpSessionEventPublisher());
+    }
+}
---
+++ src/main/java/tgb/btc/web/config/listener/SessionExpiredListener.java
@@ -0,0 +1,26 @@
+package tgb.btc.web.config.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.session.SessionDestroyedEvent;
+import org.springframework.stereotype.Component;
+import tgb.btc.library.bean.web.WebUser;
+import tgb.btc.web.controller.common.NotificationsController;
+
+@Component
+@Slf4j
+public class SessionExpiredListener implements ApplicationListener<SessionDestroyedEvent> {
+
+    @Override
+    public void onApplicationEvent(SessionDestroyedEvent event) {
+        for (SecurityContext securityContext : event.getSecurityContexts()) {
+            Authentication authentication = securityContext.getAuthentication();
+            WebUser user = (WebUser) authentication.getPrincipal();
+            NotificationsController.LISTENERS.get(user.getUsername()).complete();
+            NotificationsController.LISTENERS.remove(user.getUsername());
+        }
+    }
+
+}
