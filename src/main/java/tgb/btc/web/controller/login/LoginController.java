4eab371a4664a1ef5bc060c15a03f58b46edd560
barilo <e.barilo@isida.by>
2024-06-11 15:02:48 +0300

    center + remove instant

--- src/main/java/tgb/btc/web/controller/login/LoginController.java
+++ src/main/java/tgb/btc/web/controller/login/LoginController.java
@@ -3,32 +3,18 @@ package tgb.btc.web.controller.login;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import tgb.btc.api.web.INotifier;
-import tgb.btc.library.bean.web.Role;
-import tgb.btc.library.bean.web.WebUser;
 import tgb.btc.library.repository.web.WebUserRepository;
-import tgb.btc.library.util.web.JacksonUtil;
 import tgb.btc.web.controller.BaseController;
 import tgb.btc.web.util.RequestUtil;
-import tgb.btc.web.util.SuccessResponseUtil;
 import tgb.btc.web.vo.EmitterVO;
-import tgb.btc.web.vo.SuccessResponse;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
-
-import static org.springframework.security.web.context.HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY;
 
 @Controller
 @Slf4j
@@ -74,21 +60,21 @@ public class LoginController extends BaseController {
         return emitter;
     }
 
-    @PostMapping("/loginInstant")
-    @ResponseBody
-    public SuccessResponse<?> loginInstant(HttpServletRequest request, String login) {
-        WebUser webUser = webUserRepository.getByUsername(login);
-        String[] roles = new String[webUser.getRoles().size()];
-        int i = 0;
-        for (Role role : webUser.getRoles()) {
-            roles[i] = role.getName();
-        }
-        Authentication auth = new UsernamePasswordAuthenticationToken(webUser, null,
-                AuthorityUtils.createAuthorityList(roles)
-        );
-        SecurityContextHolder.getContext().setAuthentication(auth);
-        SecurityContext sc = SecurityContextHolder.getContext();
-        request.getSession().setAttribute(SPRING_SECURITY_CONTEXT_KEY, sc);
-        return SuccessResponseUtil.data(true, data -> JacksonUtil.getEmpty().put("success", true));
-    }
+//    @PostMapping("/loginInstant")
+//    @ResponseBody
+//    public SuccessResponse<?> loginInstant(HttpServletRequest request, String login) {
+//        WebUser webUser = webUserRepository.getByUsername(login);
+//        String[] roles = new String[webUser.getRoles().size()];
+//        int i = 0;
+//        for (Role role : webUser.getRoles()) {
+//            roles[i] = role.getName();
+//        }
+//        Authentication auth = new UsernamePasswordAuthenticationToken(webUser, null,
+//                AuthorityUtils.createAuthorityList(roles)
+//        );
+//        SecurityContextHolder.getContext().setAuthentication(auth);
+//        SecurityContext sc = SecurityContextHolder.getContext();
+//        request.getSession().setAttribute(SPRING_SECURITY_CONTEXT_KEY, sc);
+//        return SuccessResponseUtil.data(true, data -> JacksonUtil.getEmpty().put("success", true));
+//    }
 }
