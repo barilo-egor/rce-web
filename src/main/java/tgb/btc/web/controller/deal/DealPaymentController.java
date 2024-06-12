5e5822c5de6f58ae156de7248a50de4417fa30bd
barilo <barilo>
2024-06-12 20:50:20 +0300

    DealPaymentController

--- src/main/java/tgb/btc/web/controller/deal/DealPaymentController.java
+++ src/main/java/tgb/btc/web/controller/deal/DealPaymentController.java
@@ -1,5 +1,6 @@
 package tgb.btc.web.controller.deal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,11 @@ import tgb.btc.web.controller.BaseController;
 public class DealPaymentController extends BaseController {
 
     private DealPaymentRepository dealPaymentRepository;
+
+    @Autowired
+    public void setDealPaymentRepository(DealPaymentRepository dealPaymentRepository) {
+        this.dealPaymentRepository = dealPaymentRepository;
+    }
 
     @PostMapping("/new")
     public void newPayment(String title, String message, String app) {
