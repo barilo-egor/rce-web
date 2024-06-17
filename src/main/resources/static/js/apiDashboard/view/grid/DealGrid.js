fc66da3ef3d446a2166d03e854c920ec3ccae05b
barilo <e.barilo@isida.by>
2024-06-17 17:29:24 +0300

    toolbar deal grid

--- src/main/resources/static/js/apiDashboard/view/grid/DealGrid.js
+++ src/main/resources/static/js/apiDashboard/view/grid/DealGrid.js
@@ -1,64 +1,80 @@
 Ext.define('ApiDashboard.view.grid.DealGrid', {
-    extend: 'Ext.grid.Grid',
+    extend: 'Ext.Panel',
     xtype: 'dealgrid',
 
     shadow: true,
-    columnsMenuItem: null,
-    store: Ext.create('ApiDashboard.store.DealStore'),
 
-    listeners: {
-        painted: function (me) {
-            me.getStore().load()
-        }
-    },
-    plugins: {
-        pagingtoolbar: true
-    },
-
-    columns: [
-        {
-            text: '№',
-            dataIndex: 'pid',
-            width: 80
-        },
-        {
-            text: 'Статус',
-            dataIndex: 'apiDealStatus',
-            flex: 0.5,
-            cell: {
-                encodeHtml: false
-            },
-            renderer: function (val) {
-                return '<span class="' + val.color + '">' + val.description + '</span>'
+    tbar: {
+        items: [
+            {
+                iconCls: 'x-fa fa-file-excel darkGreen',
+                tooltip: 'Экспорт сделок в Excel',
             }
-        },
+        ]
+    },
+
+    layout: 'fit',
+    items: [
         {
-            text: 'Тип сделки',
-            dataIndex: 'dealType',
-            renderer: function (val) {
-                return val.displayName
+            xtype: 'grid',
+            columnsMenuItem: null,
+            store: Ext.create('ApiDashboard.store.DealStore'),
+
+            listeners: {
+                painted: function (me) {
+                    me.getStore().load()
+                }
             },
-            flex: 0.3
-        },
-        {
-            text: 'Сумма в крипте',
-            dataIndex: 'cryptoAmount',
-            flex: 0.3
-        },
-        {
-            text: 'Фиат сумма',
-            dataIndex: 'amount',
-            flex: 0.3
-        },
-        {
-            text: 'Дата и время',
-            dataIndex: 'dateTime',
-            width: 150
-        },
-        {
-            text: 'Реквизит',
-            dataIndex: 'requisite',
-            flex: 1
+            plugins: {
+                pagingtoolbar: true
+            },
+
+            columns: [
+                {
+                    text: '№',
+                    dataIndex: 'pid',
+                    width: 80
+                },
+                {
+                    text: 'Статус',
+                    dataIndex: 'apiDealStatus',
+                    flex: 0.5,
+                    cell: {
+                        encodeHtml: false
+                    },
+                    renderer: function (val) {
+                        return '<span class="' + val.color + '">' + val.description + '</span>'
+                    }
+                },
+                {
+                    text: 'Тип сделки',
+                    dataIndex: 'dealType',
+                    renderer: function (val) {
+                        return val.displayName
+                    },
+                    flex: 0.3
+                },
+                {
+                    text: 'Сумма в крипте',
+                    dataIndex: 'cryptoAmount',
+                    flex: 0.3
+                },
+                {
+                    text: 'Фиат сумма',
+                    dataIndex: 'amount',
+                    flex: 0.3
+                },
+                {
+                    text: 'Дата и время',
+                    dataIndex: 'dateTime',
+                    width: 150
+                },
+                {
+                    text: 'Реквизит',
+                    dataIndex: 'requisite',
+                    flex: 1
+                }
+            ]
         }
     ]
 })
