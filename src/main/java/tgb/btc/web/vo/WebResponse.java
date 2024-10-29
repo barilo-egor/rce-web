070c4aad43c180b279bd137df9662d5438cff6f0
barilo <e.barilo@isida.by>
2024-10-29 16:03:55 +0300

    test

--- src/main/java/tgb/btc/web/vo/WebResponse.java
+++ src/main/java/tgb/btc/web/vo/WebResponse.java
@@ -2,7 +2,7 @@ package tgb.btc.web.vo;
 
 public class WebResponse {
     private final boolean isSuccess;
-//test
+
     public WebResponse(boolean isSuccess) {
         this.isSuccess = isSuccess;
     }
