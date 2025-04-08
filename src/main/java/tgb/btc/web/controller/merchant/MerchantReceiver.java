00385c0a5722ed62f7a061088bdafba734303588
barilo <barilo>
2025-04-08 23:41:35 +0300

    payfinity callback

--- src/main/java/tgb/btc/web/controller/merchant/MerchantReceiver.java
+++ src/main/java/tgb/btc/web/controller/merchant/MerchantReceiver.java
@@ -1,14 +1,18 @@
 package tgb.btc.web.controller.merchant;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import tgb.btc.library.service.web.merchant.alfateam.AlfaTeamMerchantService;
+import tgb.btc.library.service.web.merchant.payfinity.PayFinityMerchantService;
 import tgb.btc.library.vo.web.merchant.alfateam.InvoiceNotification;
 
 import java.util.Objects;
@@ -22,10 +26,13 @@ public class MerchantReceiver {
 
     private final AlfaTeamMerchantService alfaTeamMerchantService;
 
+    private final PayFinityMerchantService payFinityMerchantService;
+
     public MerchantReceiver(@Value("${alfateam.api.notification.token}") String alfaTeamNotificationToken,
-            AlfaTeamMerchantService alfaTeamMerchantService) {
+                            AlfaTeamMerchantService alfaTeamMerchantService, PayFinityMerchantService payFinityMerchantService) {
         this.alfaTeamNotificationToken = alfaTeamNotificationToken;
         this.alfaTeamMerchantService = alfaTeamMerchantService;
+        this.payFinityMerchantService = payFinityMerchantService;
     }
 
     @PostMapping("/alfateam")
@@ -38,5 +45,17 @@ public class MerchantReceiver {
         } else {
             return ResponseEntity.badRequest().build();
         }
+    }
+
+    @PostMapping("/payfinity")
+    @ResponseStatus(HttpStatus.OK)
+    public void payFinity(@RequestBody PayFinityRequest payFinityRequest) {
+        payFinityMerchantService.updateStatus(payFinityRequest.getTrackerId());
+    }
+
+    @Data
+    public static class PayFinityRequest {
+
+        private String trackerId;
     }
 }
